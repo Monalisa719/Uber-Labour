e2c729e765606513b26709fedba4263471bcd088
Monalisa <monalisabxr@gmail.com>
2026-05-14 23:14:08 +0530

    fixed cors issue

--- server/app.js
+++ server/app.js
@@ -18,7 +18,11 @@ const server = http.createServer(app);
 
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", process.env.FRONTEND_URL];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://uber-labour-roan.vercel.app"
+];
 
 app.use(
   cors({
